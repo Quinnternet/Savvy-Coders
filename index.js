7ea6dbd4b834a0b4fa7cc932c5c69eb081da2fe8
Quinn Darling <code@quinntessential.33mail.com>
2019-06-24 23:39:45 -0500

    :hammer: Small change to index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import Fill from './components/calendar/FillCalendar'
 import Feed from './components/rss/RSS'
+import Social from './components/social/Social'
 // import Weather from './components/Weather'
 
 
 Fill()
 Feed()
+Social()
 // Weather()
